c2d2f5cf076ae65d1bfa2a8ff5bb7106287598ae
Ayush Aggarwal <ayusha@successive.com>
2020-04-02 17:42:49 +0530

    .

--- MobApp/MobApp/MobApp/Login.xaml.cs
+++ MobApp/MobApp/MobApp/Login.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobApp.ModelView;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,69 +19,70 @@ namespace MobApp
         public Login()
         {
             InitializeComponent();
+            BindingContext = new LoginViewModel();
         }
-        void OnButtonClicked(object sender, EventArgs args)
-        {
-            var inputEmail = email.Text;
-            var inputPassword = password.Text;
-            if ((""+email.Text).Length == 0 || (""+password.Text).Length == 0)
-            {
-                DisplayAlert("Error", "Please fill all the fields ", "ok");
-            }
-            else if (inputEmail != Email || inputPassword != Password)
-            {
-                DisplayAlert("Error", "Provide the correct input ", "ok");
-            }
-            else if (inputEmail == Email && inputPassword == Password)
-            {
-                DisplayAlert("Login", "Success", "ok");
-            }
-        }
+        //void OnButtonClicked(object sender, EventArgs args)
+        //{
+        //    var inputEmail = email.Text;
+        //    var inputPassword = password.Text;
+        //    if ((""+email.Text).Length == 0 || (""+password.Text).Length == 0)
+        //    {
+        //        DisplayAlert("Error", "Please fill all the fields ", "ok");
+        //    }
+        //    else if (inputEmail != Email || inputPassword != Password)
+        //    {
+        //        DisplayAlert("Error", "Provide the correct input ", "ok");
+        //    }
+        //    else if (inputEmail == Email && inputPassword == Password)
+        //    {
+        //        DisplayAlert("Login", "Success", "ok");
+        //    }
+        //}
 
-        private void Email_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            var oldText =""+e.OldTextValue;
-            var newText = e.NewTextValue;
+        //private void Email_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    var oldText =""+e.OldTextValue;
+        //    var newText = e.NewTextValue;
            
-            if (oldText.Length==0)
-            {
-                if (Char.Parse(newText) < 65 || Char.Parse(newText) > 90&& Char.Parse(newText)<97 || Char.Parse(newText) >122)
-                {
-                    DisplayAlert("Error", "Invalid Format", "ok");
-                }
-            }
-            else if (oldText.Contains("@") && newText == "@")
-            {
-                DisplayAlert("Error", "Invalid format", "ok");
-            }
-        }
+        //    if (oldText.Length==0)
+        //    {
+        //        if (Char.Parse(newText) < 65 || Char.Parse(newText) > 90&& Char.Parse(newText)<97 || Char.Parse(newText) >122)
+        //        {
+        //            DisplayAlert("Error", "Invalid Format", "ok");
+        //        }
+        //    }
+        //    else if (oldText.Contains("@") && newText == "@")
+        //    {
+        //        DisplayAlert("Error", "Invalid format", "ok");
+        //    }
+        //}
 
-        private void Email_Completed(object sender, EventArgs e)
-        {
-            var inputEmail = ""+email.Text;
-            Regex regex = new Regex(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
-            bool isValid = regex.IsMatch((inputEmail.ToString()).Trim());
-            if (!isValid)
-            {
-                DisplayAlert("Error", "Please enter the valid email", "ok");
-            }
-            else 
-            {
-                password.Focus();           
-            }
-        }
-        private void Password_Completed(object sender, EventArgs e)
-        {
-            var inputPassword = "" + password.Text;
+        //private void Email_Completed(object sender, EventArgs e)
+        //{
+        //    var inputEmail = ""+email.Text;
+        //    Regex regex = new Regex(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+        //    bool isValid = regex.IsMatch((inputEmail.ToString()).Trim());
+        //    if (!isValid)
+        //    {
+        //        DisplayAlert("Error", "Please enter the valid email", "ok");
+        //    }
+        //    else 
+        //    {
+        //        password.Focus();           
+        //    }
+        //}
+        //private void Password_Completed(object sender, EventArgs e)
+        //{
+        //    var inputPassword = "" + password.Text;
         
-            if (inputPassword.Length==0)
-            {
-                DisplayAlert("Error", "Please provide the password", "ok");
-            }
-            else
-            {
-                login.Focus();
-            }
-        }
+        //    if (inputPassword.Length==0)
+        //    {
+        //        DisplayAlert("Error", "Please provide the password", "ok");
+        //    }
+        //    else
+        //    {
+        //        login.Focus();
+        //    }
+        //}
     }
 }
---
+++ MobApp/MobApp/MobApp/ModelView/LoginViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace MobApp.ModelView
+{
+    public class LoginViewModel
+    {
+        const string ConstEmail = "ayush@gmail.com";
+        const string ConstPassword = "1234";
+        private string _email;
+        private string _password;
+        private string _txt;
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                value = _email;
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                value = _password;
+            }
+        }
+        public string Txt
+        {
+            get
+            {
+                return _txt;
+            }
+            set
+            {
+                value = _txt;
+            }
+        }
+        public ICommand OnLogin;
+       public LoginViewModel()
+        {
+            OnLogin = new Command(OnButtonClicked);
+        }
+        void OnButtonClicked()
+        {
+            var inputEmail = Email;
+            var inputPassword = Password;
+            if (("" + Email).Length == 0 || ("" + Password).Length == 0)
+            {
+                Txt = "ayush";
+            }
+            //else if (Email != ConstEmail || Password != ConstPassword)
+            //{
+            //    DisplayAlert("Error", "Provide the correct input ", "ok");
+            //}
+            //else if (Email == ConstEmail && Password == ConstPassword)
+            //{
+            //    DisplayAlert("Login", "Success", "ok");
+            //}
+
+        }
+    }
+}

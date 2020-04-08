d7c54020e917af23e30e1592c0d9c498dce18198
Ayush Aggarwal <ayusha@successive.com>
2020-04-08 17:57:19 +0530

    Different pages MasterDetail.Tabbed in MaterDetail

---
+++ TryApp/TryApp/TryApp/PracMasterDetail.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TryApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PracMasterDetail : MasterDetailPage
+    {
+        public PracMasterDetail()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Page1());
+            IsPresented = false;
+        }
+        private void Button_Clicked2(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new PracTabbedPage());
+            IsPresented = false;
+        }
+        private void Button_Clicked3(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new PracCarouselPage());
+            IsPresented = false;
+        }
+    }
+}

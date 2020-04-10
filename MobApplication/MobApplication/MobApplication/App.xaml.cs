337076633467677c4240ef3ba1151e0750d0a3f4
Ayush Aggarwal <ayusha@successive.com>
2020-04-10 18:49:12 +0530

    Content Views,crash Analytic

--- MobApplication/MobApplication/MobApplication/App.xaml.cs
+++ MobApplication/MobApplication/MobApplication/App.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using MobApplication.View;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 namespace MobApplication
 {
     public partial class App : Application
@@ -10,11 +13,15 @@ namespace MobApplication
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new MainPage1();
         }
 
         protected override void OnStart()
         {
+            AppCenter.Start("5139bcae-32d9-4bbf-acfa-712c21577886",
+                   typeof(Analytics), typeof(Crashes));
+            AppCenter.Start("5139bcae-32d9-4bbf-acfa-712c21577886",
+                               typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
---
+++ MobApplication/MobApplication/MobApplication/View/MainPage1.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace MobApplication.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage1 : MasterDetailPage
+    {
+        public MainPage1()
+        {
+            InitializeComponent();
+            contentview.Content = new View.AboutUs();
+        }
+
+        private void Login_Clicked(object sender, EventArgs e)
+        {
+            //tried crash analytics
+            //var n = 0;
+            //int a = n / 0;
+            App.Current.MainPage = new Login();
+        }
+
+        private void ContactUs_Clicked(object sender, EventArgs e)
+        {
+            contentview.Content = new View.contactus();
+        }
+
+        private void AboutUs_Clicked(object sender, EventArgs e)
+        {
+            contentview.Content = new View.AboutUs();
+
+        }
+    }
+}

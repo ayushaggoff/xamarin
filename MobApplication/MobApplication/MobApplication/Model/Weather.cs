c13eb8cb74f447b9db232f09087686626f78d8f3
Ayush Aggarwal <ayusha@successive.com>
2020-04-13 20:32:40 +0530

    created a weather class for list view

---
+++ MobApplication/MobApplication/MobApplication/Model/Weather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobApplication.Model
+{
+    public class Weather
+    {
+        public string State { get; set; }
+        public string WeatherCond { get; set; }
+        public string Temprature { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}

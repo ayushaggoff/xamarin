501eaed10daafae72f910b6f8edaf04dd8700f5d
Ayush Aggarwal <ayusha@successive.com>
2020-04-19 11:52:23 +0530

    Weather Info: On Select Change background

--- MobApplication/MobApplication/MobApplication/Model/Weather.cs
+++ MobApplication/MobApplication/MobApplication/Model/Weather.cs
@@ -1,14 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
 using System.Text;
 
 namespace MobApplication.Model
 {
-    public class Weather
+    public class Weather: INotifyPropertyChanged
     {
         public string State { get; set; }
         public string WeatherCond { get; set; }
         public string Temprature { get; set; }
         public string ImageUrl { get; set; }
+         Color _color;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Color bgColor 
+        {
+            get
+            {
+                return _color;
+            }
+            set
+            {
+                _color = value;
+                OnPropertyChanged("bgColor");
+            }
+        }
+
+
+        private void OnPropertyChanged(string status)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(status));
+            }
+        }
     }
 }
--- MobApplication/MobApplication/MobApplication/View/WeatherInformation.xaml.cs
+++ MobApplication/MobApplication/MobApplication/View/WeatherInformation.xaml.cs
@@ -13,13 +13,9 @@ namespace MobApplication.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WeatherInformation : ContentPage
     {
+        Weather pepe;
         ViewCell lastCell;
-        ObservableCollection<Weather> list = new ObservableCollection<Weather>();
-        public WeatherInformation()
-        {
-            InitializeComponent();
-
-            list = new ObservableCollection<Weather>
+        ObservableCollection<Weather> list = new ObservableCollection<Weather>
             {
             new Weather{ State="Berlin", WeatherCond="Snowing",Temprature="0\u2103" ,ImageUrl="https://img.icons8.com/offices/40/000000/snow.png"},
             new Weather { State="Bangalore", WeatherCond="Thunderstorms",Temprature="23\u2103",ImageUrl="https://img.icons8.com/offices/40/000000/storm.png" },
@@ -27,14 +23,28 @@ namespace MobApplication.View
             new Weather { State="New York", WeatherCond="Cloudy",Temprature="18\u2103" ,ImageUrl="https://img.icons8.com/offices/40/000000/clouds.png"},
             new Weather { State="Sydney", WeatherCond="Sunny",Temprature="6\u2103" ,ImageUrl="https://img.icons8.com/offices/40/000000/sun.png"}
             };
+        public WeatherInformation()
+        {
+            InitializeComponent();
+
+            BindingContext = new Weather();
             listView.ItemsSource = list;
+           pepe = list[0];//
             // BindableLayout.SetItemsSource(g1, list);
         }
 
         private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
+            
+            var se = (ListView)sender;
+            pepe.bgColor = Color.Transparent;
+            int count = 0;
+            pepe.bgColor = Color.Transparent;
+            var data = e.SelectedItem as Weather;
 
-            var state = e.SelectedItemIndex;
+            data.bgColor = Color.Red;
+            pepe = data;
+            //var state = e.SelectedItemIndex;
             //     list.RemoveAt(state);
 
         }
@@ -55,6 +65,11 @@ namespace MobApplication.View
                 viewCell.View.BackgroundColor = Color.Purple;
                 lastCell = viewCell;
             }
+        }
+
+        private void listView_ItemSelected_1(object sender, SelectedItemChangedEventArgs e)
+        {
+
         }
     }
 }

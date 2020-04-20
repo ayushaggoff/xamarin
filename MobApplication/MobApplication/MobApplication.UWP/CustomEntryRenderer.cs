358cb6e3d6a4327c72841aaf21703e387204ed98
Ayush Aggarwal <ayusha@successive.com>
2020-04-20 19:47:38 +0530

    Entry : Custom Renderers

---
+++ MobApplication/MobApplication/MobApplication.UWP/CustomEntryRenderer.cs
@@ -0,0 +1,27 @@
+﻿using MobApplication.UWP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MobApplication.Control;
+using Xamarin.Forms.Platform.UWP;
+using Xamarin.Forms;
+
+[assembly: ExportRenderer(typeof(CustomEntry), typeof(CustomEntryRenderer))]
+namespace MobApplication.UWP
+{
+    public class CustomEntryRenderer: EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+                
+            {
+                Control.BorderBrush.Opacity = 0;
+            }
+        }
+    }
+}

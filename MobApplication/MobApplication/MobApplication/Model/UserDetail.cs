0d33d97107f55f91426d7f8b914916ac941fc209
Ayush Aggarwal <ayusha@successive.com>
2020-04-21 20:35:08 +0530

    Created: Detail Page

---
+++ MobApplication/MobApplication/MobApplication/Model/UserDetail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobApplication.Model
+{
+    public class UserDetail
+    {
+        public string ProfileCreatedDate{get;set;}
+        public int AccountNumber { get; set;}
+        public string Name { get; set;}
+        public string Type { get; set;}
+        public string Email { get; set;}
+        public long PhoneNumber { get; set;}
+        public string Location { get; set; }
+       public  UserDetail()
+        {
+            ProfileCreatedDate = "18 NOV 2018";
+            AccountNumber = 12345678;
+            Name="Ayush Aggarwal";
+            Type = "Vendor";
+            Email = "ayushagg@gmail.com";
+            PhoneNumber = 9736826298;
+            Location = "E 29 Noida, India";
+        }
+
+    }
+}
---
+++ MobApplication/MobApplication/MobApplication/View/DetailPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using MobApplication.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobApplication.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailPage : ContentPage
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            BindingContext =new  UserDetail();
+        }
+    }
+}

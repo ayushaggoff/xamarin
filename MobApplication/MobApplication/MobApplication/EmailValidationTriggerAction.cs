b9d35537e8538bec58b131b3c3acc44f322f5f38
Ayush Aggarwal <ayusha@successive.com>
2020-04-23 17:55:57 +0530

    action trigger

---
+++ MobApplication/MobApplication/MobApplication/EmailValidationTriggerAction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using Xamarin.Forms;
+
+namespace MobApplication
+{
+    class EmailValidationTriggerAction : TriggerAction<Entry>
+    {
+        protected override void Invoke(Entry entry)
+        {
+            var email = entry.Text;
+            Regex regex = new Regex(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+            bool isValid = regex.IsMatch(("" + email).Trim());
+            if (!isValid)
+            {
+                entry.BackgroundColor = Color.Tomato;
+                App.Current.MainPage.DisplayAlert("Error", "please enter the valid email", "ok");
+            }
+            else
+            {
+                entry.BackgroundColor = Color.LightGreen;
+            }
+        }
+    }
+}

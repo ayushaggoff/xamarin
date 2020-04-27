13de6219d33032c84b0bf97585a257574bfd5d7d
Ayush Aggarwal <ayusha@successive.com>
2020-04-27 19:40:26 +0530

    Android.App: Effects in Xamarin.Forms

---
+++ MobApplication/MobApplication/MobApplication/Effect/RoundEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MobApplication.Effect
+{
+    public class RoundEffect : RoutingEffect
+    {
+        public RoundEffect() : base($"Xamarin.{nameof(RoundEffect)}")
+        {
+        }
+    }
+}

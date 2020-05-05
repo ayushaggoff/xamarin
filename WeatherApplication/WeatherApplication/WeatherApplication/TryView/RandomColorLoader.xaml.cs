ecb51483af66f925979b70e3dcf822114bac46f9
Ayush Aggarwal <ayusha@successive.com>
2020-05-05 19:44:44 +0530

    RandomColorLoader

---
+++ WeatherApplication/WeatherApplication/WeatherApplication/TryView/RandomColorLoader.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApplication.TryView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RandomColorLoader : ContentView
+    {
+        Random random = new Random();
+        public RandomColorLoader()
+        {
+            InitializeComponent();
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                custombox.Color = customlbl.TextColor = customsg.TextColor =
+                     customloader.Color =
+                Color.FromRgb(random.Next(0, 255), random.Next(0, 255), random.Next(0, 255));
+                // True = Repeat again, False = Stop the timer  
+                return true;
+            });
+        }
+    }
+}

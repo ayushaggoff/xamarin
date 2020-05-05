009cdd308b873c1d11bd75f4bf34c4e95b0e62c2
Ayush Aggarwal <ayusha@successive.com>
2020-05-05 19:46:55 +0530

    CustomCircle DesignLoader

---
+++ WeatherApplication/WeatherApplication/WeatherApplication/TryView/CustomGifLoader.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WeatherApplication.TryView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomGifLoader : ContentView
+    {
+        public CustomGifLoader()
+        {
+            InitializeComponent();
+        }
+    }
+}

4809748fecf5f97a831819d2ef1e96bc0cb9efa9
Ayush Aggarwal <ayusha@successive.com>
2020-05-12 19:07:20 +0530

    Custom Nav Bar

--- WeatherApplication/WeatherApplication/WeatherApplication/TryView/CustomNavBar.xaml.cs
+++ WeatherApplication/WeatherApplication/WeatherApplication/TryView/CustomNavBar.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
+using Xamarin.Forms.Markup;
 using Xamarin.Forms.Xaml;
 
 namespace WeatherApplication.TryView
@@ -13,13 +14,13 @@ namespace WeatherApplication.TryView
     public partial class CustomNavBar : ContentView
     {
         public static readonly BindableProperty LabelTitleTextProperty = BindableProperty.Create("Title_Text",
-           typeof(string), typeof(CustomNavBar), "", BindingMode.TwoWay, propertyChanged: LabelTitleTextValueChanged); 
-        
+           typeof(string), typeof(CustomNavBar), "", BindingMode.TwoWay, propertyChanged: LabelTitleTextValueChanged);
+
         public static readonly BindableProperty LabelTitleTextColorProperty = BindableProperty.Create("Title_TextColor",
-           typeof(string), typeof(CustomNavBar), "", BindingMode.TwoWay, propertyChanged: LabelTitleTextColorValueChanged); 
-        
+           typeof(Color), typeof(CustomNavBar),Color.Black, BindingMode.TwoWay, propertyChanged: LabelTitleTextColorValueChanged);
+
         public static readonly BindableProperty LabelTitleBackgroundColorProperty = BindableProperty.Create("Title_BackgroundColor",
-           typeof(string), typeof(CustomNavBar), "", BindingMode.TwoWay, propertyChanged: LabelTitleBackgroundColorValueChanged);
+           typeof(Color), typeof(CustomNavBar), Color.Transparent, BindingMode.TwoWay, propertyChanged: LabelTitleBackgroundColorValueChanged);
 
         public static readonly BindableProperty BtnLeftImageSourceProperty = BindableProperty.Create("LeftImage_Source",
            typeof(string), typeof(CustomNavBar), "", BindingMode.TwoWay, propertyChanged: BtnLeftImageSourcePropertyValueChanged);
@@ -29,21 +30,57 @@ namespace WeatherApplication.TryView
 
         public static readonly BindableProperty BtnRightImg1SourceProperty = BindableProperty.Create("RightImage1_Source",
            typeof(string), typeof(CustomNavBar), "", BindingMode.TwoWay, propertyChanged: BtnRightImg1SourcePropertyValueChanged);
+        //public static BindableProperty CommandProperty = BindableProperty.Create("Command",
+        //  typeof(ICommand),
+        //  typeof(CustomNavBar), default(ICommand), BindingMode.TwoWay,
+        //  propertyChanged: (bindable, oldVal, newval) => {
+        //      var b = (bindable as CustomNavBar);
+        //      b.Btn_LeftImg.Command = new Command(() =>
+        //      {
+
+        //          //if (b.Btn_LeftImg.Command != null)
+        //          //{
+        //          //    Clicked?.Invoke(this, EventArgs.Empty);
+        //          //    if (b.Btn_LeftImg.Command.CanExecute("test"))
+        //          //    {
+        //          //        b.Btn_LeftImg.Command.Execute("test");
+
+        //          //        return;
+        //          //    }
+        //          //}
+
+        //      });
+        //  });
+
+        //public static BindableProperty CommandLeftImageProperty = BindableProperty.Create("Command",
+        //    typeof(ICommand),
+        //    typeof(CustomNavBar),BindingMode.TwoWay,
+        //    propertyChanged: (bindable, oldVal, newval) => {
+        //        var b = (bindable as CustomNavBar);
+        //        b.Btn_LeftImg.Command = new Command(() =>
+        //        {
+        //            if (b.Btn_LeftImg.Command != null)
+        //            {
+        //                if (b.Btn_LeftImg.Command.CanExecute(""))
+        //                    b.Btn_LeftImg.Command.Execute("");
+        //            }
+
+        //        });
+        //    });
+        public static void LeftButtonCommandPropertyChanged(object bindable,object  oldVal,object newval)
+        {
+
+        }
 
 
-        public static readonly BindableProperty GridNavBarBackgroundProperty = BindableProperty.Create("NavBar_Background",
-           typeof(string), typeof(CustomNavBar), "", BindingMode.TwoWay, propertyChanged: GridNavBarBackgroundPropertyValueChanged);
+        public static BindableProperty TestCommandProperty = BindableProperty.Create("TestCommand",
+          typeof(ICommand),
+         typeof(CustomNavBar), null,propertyChanged: LeftButtonCommandPropertyChanged);
 
-
-        public static readonly BindableProperty CommandLeftImageProperty = BindableProperty.Create(nameof(Command_LeftImage),
+        public static readonly BindableProperty CommandRightImageOneProperty = BindableProperty.Create("Command_RightImageOne",
             typeof(ICommand),
             typeof(CustomNavBar),
-            default(ICommand));
-
-        public static readonly BindableProperty CommandRightImage1Property = BindableProperty.Create(nameof(Command_RightImage1),
-            typeof(ICommand),
-            typeof(CustomNavBar),
-            default(ICommand));
+            null);
 
         public static readonly BindableProperty CommandRightImage2Property = BindableProperty.Create(nameof(Command_RightImage2),
             typeof(ICommand),
@@ -54,43 +91,79 @@ namespace WeatherApplication.TryView
         public static readonly BindableProperty CommandParameterLeftImageProperty = BindableProperty.Create(nameof(CommandParameter_LeftImage),
         typeof(object),
         typeof(CustomNavBar));
-        public static readonly BindableProperty CommandParameterRightImage1Property = BindableProperty.Create(nameof(CommandParameter_RightImage1),
+        public static readonly BindableProperty CommandParameterRightImageOneProperty = BindableProperty.Create(nameof(CommandParameter_RightImageOne),
         typeof(object),
         typeof(CustomNavBar));
         public static readonly BindableProperty CommandParameterRightImage2Property = BindableProperty.Create(nameof(CommandParameter_RightImage2),
         typeof(object),
         typeof(CustomNavBar));
+        public static event EventHandler Clicked ; 
         public CustomNavBar()
         {
             InitializeComponent();
 
-            Content.BindingContext = this;
+           // TapGestureRecognizer tap = new TapGestureRecognizer();
+           // tap.BindCommand("TestCommand");
+            //Btn_LeftImg.GestureRecognizers.Add(new TapGestureRecognizer
+            //{
+            //    Command = new Command(() =>
+            //    {
+            //        Clicked?.Invoke(this, EventArgs.Empty);
+            //        if (Command != null)
+            //        {
+            //            if (Command.CanExecute(CommandParameter_LeftImage))
+            //                Command.Execute(CommandParameter_LeftImage);
+            //        }
+
+            //    })
+            //});
+
+
+            //Btn_RightImg1.GestureRecognizers.Add(new TapGestureRecognizer
+            //{
+            //    Command = new Command(() =>
+            //    {
+            //        Clicked?.Invoke(this, EventArgs.Empty);
+            //        if (Command != null)
+            //        {
+            //            if (Command.CanExecute(CommandParameter_RightImageOne))
+            //                Command.Execute(CommandParameter_RightImageOne);
+            //        }
+
+            //    })
+            //});
+
         }
 
-        public ICommand Command_LeftImage
+        //public ICommand Command 
+        //{
+        //    get => (ICommand)GetValue(CommandProperty);
+        //    set => SetValue(CommandProperty, value);
+        //}
+        //public ICommand Command_LeftImage
+        //{
+        //    get => (ICommand)GetValue(CommandLeftImageProperty);
+        //    set => SetValue(CommandLeftImageProperty, value);
+        //}
+        public ICommand TestCommand
         {
-            get => (ICommand)GetValue(CommandLeftImageProperty);
-            set => SetValue(CommandLeftImageProperty, value);
-        }
-        public ICommand Command_RightImage1
-        {
-            get => (ICommand)GetValue(CommandRightImage1Property);
-            set => SetValue(CommandRightImage1Property, value);
+            get => (ICommand)GetValue(TestCommandProperty);
+            set => SetValue(TestCommandProperty, value);
         }
         public ICommand Command_RightImage2
         {
             get => (ICommand)GetValue(CommandRightImage2Property);
             set => SetValue(CommandRightImage2Property, value);
         }
-        public object CommandParameter_LeftImage
+        public Object CommandParameter_LeftImage
         {
             get => GetValue(CommandParameterLeftImageProperty);
             set => SetValue(CommandParameterLeftImageProperty, value);
         }
-        public object CommandParameter_RightImage1
+        public object CommandParameter_RightImageOne
         {
-            get => GetValue(CommandParameterRightImage1Property);
-            set => SetValue(CommandParameterRightImage1Property, value);
+            get => GetValue(CommandParameterRightImageOneProperty);
+            set => SetValue(CommandParameterRightImageOneProperty, value);
         }
         public object CommandParameter_RightImage2
         {
@@ -102,14 +175,14 @@ namespace WeatherApplication.TryView
             get { return (string)GetValue(LabelTitleTextProperty); }
             set { SetValue(LabelTitleTextProperty, value); }
         }
-        public string Title_TextColor
+        public Color Title_TextColor
         {
-            get { return (string)GetValue(LabelTitleTextColorProperty); }
+            get { return (Color)GetValue(LabelTitleTextColorProperty); }
             set { SetValue(LabelTitleTextColorProperty, value); }
         }
-        public string Title_BackgroundColor
+        public Color Title_BackgroundColor
         {
-            get { return (string)GetValue(LabelTitleBackgroundColorProperty); }
+            get { return (Color)GetValue(LabelTitleBackgroundColorProperty); }
             set { SetValue(LabelTitleBackgroundColorProperty, value); }
         }
         public string LeftImage_Source
@@ -127,19 +200,36 @@ namespace WeatherApplication.TryView
             get { return (string)GetValue(BtnRightImg2SourceProperty); }
             set { SetValue(BtnRightImg2SourceProperty, value); }
         }
-        public string NavBar_Background
+        public LayoutOptions Title_HorizontalOption
         {
-            get { return (string)GetValue(GridNavBarBackgroundProperty); }
-            set { SetValue(GridNavBarBackgroundProperty, value); }
+            get 
+            { 
+                return (LayoutOptions)GetValue(BtnRightImg2SourceProperty); 
+            }
+            set { 
+                SetValue(BtnRightImg2SourceProperty, value);
+                if (value.Alignment.ToString() == "Start")
+                {
+                    Lbl_LeftTitle.IsVisible = true;
+                    Lbl_Title.IsVisible = false;
+                }
+                else
+                {
+                    Lbl_LeftTitle.IsVisible = false;
+                    Lbl_Title.IsVisible = true;
+                }
+            }
         }
+
 
         public static void LabelTitleTextValueChanged(BindableObject bindable, object oldValue, object newValue)
         {
             (bindable as CustomNavBar).Lbl_Title.Text = Convert.ToString(newValue);
+            (bindable as CustomNavBar).Lbl_LeftTitle.Text = Convert.ToString(newValue);
         }
         public static void LabelTitleTextColorValueChanged(BindableObject bindable, object oldValue, object newValue)
         {
-            (bindable as CustomNavBar).Lbl_Title.TextColor =(Color)newValue;
+            (bindable as CustomNavBar).Lbl_Title.TextColor =(Color)(newValue);
         }
         public static void LabelTitleBackgroundColorValueChanged(BindableObject bindable, object oldValue, object newValue)
         {
@@ -157,10 +247,7 @@ namespace WeatherApplication.TryView
         {
             (bindable as CustomNavBar).Btn_RightImg2.Source = Convert.ToString(newValue);
         }
-        public static void GridNavBarBackgroundPropertyValueChanged(BindableObject bindable, object oldValue, object newValue)
-        {
-            (bindable as CustomNavBar).NavBar.BackgroundColor = (Color)newValue;
-        }
+      
 
     }
 }

a6ee77880027658401e0a714ed23265e01d17051
Ayush Aggarwal <ayusha@successive.com>
2020-06-15 18:31:40 +0530

    on click UWP notification & headup notification

---
+++ MApp/MApp/MApp.UWP/App.xaml.cs
@@ -0,0 +1,241 @@
+﻿using Microsoft.QueryStringDotNET;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace MApp.UWP
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                Xamarin.Forms.Forms.Init(e);
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+
+
+        protected override async void OnActivated(IActivatedEventArgs e)
+        {
+            await OnLaunchedOrActivated(e);
+        }
+
+
+
+        private async Task OnLaunchedOrActivated(IActivatedEventArgs e)
+        {
+            // Initialize things like registering background task before the app is loaded
+           
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                this.DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    // TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            // Handle toast activation
+            if (e is ToastNotificationActivatedEventArgs)
+            {
+                var toastActivationArgs = e as ToastNotificationActivatedEventArgs;
+
+                // If empty args, no specific action (just launch the app)
+                if (toastActivationArgs.Argument.Length == 0)
+                {
+                    if (rootFrame.Content == null)
+                        rootFrame.Navigate(typeof(MainPage));
+                }
+
+                // Otherwise an action is provided
+                else
+                {
+                    // Parse the query string
+                    QueryString args = QueryString.Parse(toastActivationArgs.Argument);
+
+                    // See what action is being requested 
+                    switch (args["action"])
+                    {
+                        // Open the image
+                       // case "viewImage":
+
+                            // The URL retrieved from the toast args
+                         //   string imageUrl = args["imageUrl"];
+
+                            // If we're already viewing that image, do nothing
+                            //if (rootFrame.Content is ImagePage && (rootFrame.Content as ImagePage).ImageUrl.Equals(imageUrl))
+                            //    break;
+
+                            //// Otherwise navigate to view it
+                            //rootFrame.Navigate(typeof(ImagePage), imageUrl);
+                            //break;
+
+
+                        // Open the conversation
+                        case "viewConversation":
+
+                            // The conversation ID retrieved from the toast args
+                          // int conversationId = int.Parse(args["messa"]);
+
+                            // If we're already viewing that conversation, do nothing
+                            //if (rootFrame.Content is DashboardPage && (rootFrame.Content as DashboardPage).ConversationId == conversationId)
+                            //    break;
+
+                            // Otherwise navigate to view it
+                           // rootFrame.Navigate(typeof(DashboardPage), conversationId);
+                            rootFrame.Navigate(typeof(DashboardPage));
+                            break;
+
+
+                        default:
+                            throw new NotImplementedException();
+                    }
+
+                    // If we're loading the app for the first time, place the main page on the back stack
+                    // so that user can go back after they've been navigated to the specific page
+                    if (rootFrame.BackStack.Count == 0)
+                        rootFrame.BackStack.Add(new PageStackEntry(typeof(MainPage), null, null));
+                }
+            }
+
+            // Handle launch activation
+            else if (e is LaunchActivatedEventArgs)
+            {
+                var launchActivationArgs = e as LaunchActivatedEventArgs;
+
+                // If launched with arguments (not a normal primary tile/applist launch)
+                if (launchActivationArgs.Arguments.Length > 0)
+                {
+                    // TODO: Handle arguments for cases like launching from secondary Tile, so we navigate to the correct page
+                    throw new NotImplementedException();
+                }
+
+                // Otherwise if launched normally
+                else
+                {
+                    // If we're currently not on a page, navigate to the main page
+                    if (rootFrame.Content == null)
+                        rootFrame.Navigate(typeof(MainPage));
+                }
+            }
+
+            else
+            {
+                // TODO: Handle other types of activation
+                throw new NotImplementedException();
+            }
+
+
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+
+
+
+    }
+}
--- MApp/MApp/MApp.UWP/UWPNotificationManager.cs
+++ MApp/MApp/MApp.UWP/UWPNotificationManager.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Toolkit.Uwp.Notifications;
+//using NotificationsExtensions.Toasts;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Notifications;
 using Microsoft.QueryStringDotNET; // QueryString.NET
-
-
+using System.Xml.Linq;
 
 [assembly: Xamarin.Forms.Dependency(typeof(MApp.UWP.UWPNotificationManager))]
 namespace MApp.UWP
@@ -35,97 +36,150 @@ namespace MApp.UWP
 
         public int ScheduleNotification(string title, string message)
         {
-            ToastVisual visual = new ToastVisual()
+            #region cdcwcwc
+            ToastContent tc = new ToastContent()
             {
-                BindingGeneric = new ToastBindingGeneric()
+                Visual = new ToastVisual()
                 {
-                    Children =
+                    BindingGeneric = new ToastBindingGeneric()
                     {
-                        new AdaptiveText()
+                        Children =
                         {
-                            Text = title
+                            new AdaptiveText()
+                            {
+                                Text = title
+                            },
+                            new AdaptiveText()
+                            {
+                                Text = content
+                            },
+                            new AdaptiveImage()
+                            {
+                                 Source = image
+                            }
                         },
-
-                        new AdaptiveText()
+                        AppLogoOverride = new ToastGenericAppLogo()
                         {
-                            Text = content
-                         },
-
-                        new AdaptiveImage()
-                         {
-                             Source = image
+                            Source = logo,
+                            HintCrop = ToastGenericAppLogoCrop.Circle
                         }
-                      },
-                    AppLogoOverride = new ToastGenericAppLogo()
-                    {
-                        Source = logo,
-                        HintCrop = ToastGenericAppLogoCrop.Circle
                     }
                 }
             };
 
 
-            ToastActionsCustom actions = new ToastActionsCustom()
-            {
-                Inputs =
-                {
-                    new ToastTextBox("tbReply")
-                    {
-                        PlaceholderContent = "Type a response"
-                    }
-                },
-                Buttons =
-                {
-                    new ToastButton("Reply", new QueryString()
-                    {
-                        { "action", "reply" },
-                        { "conversationId", messageId.ToString() }
+            //ToastActionsCustom actions = new ToastActionsCustom()
+            //{
+            //    Inputs =
+            //    {
+            //        new ToastTextBox("tbReply")
+            //        {
+            //            PlaceholderContent = "Type a response"
+            //        }
+            //    },
+            //    Buttons =
+            //    {
+            //        new ToastButton("Reply", new QueryString()
+            //        {
+            //            { "action", "reply" },
+            //            { "conversationId", messageId.ToString() }
 
-                    }.ToString())
-                    {
-                        ActivationType = ToastActivationType.Background,
-                        ImageUri = "Assets/Reply.png",
- 
-                        // Reference the text box's ID in order to
-                        // place this button next to the text box
-                        TextBoxId = "tbReply"
-                    },
+            //        }.ToString())
+            //        {
+            //            ActivationType = ToastActivationType.Background,
+            //            ImageUri = "Assets/Reply.png",
 
-                    new ToastButton("Like", new QueryString()
-                    {
-                        { "action", "like" },
-                        { "conversationId", messageId.ToString() }
+            //            // Reference the text box's ID in order to
+            //            // place this button next to the text box
+            //            TextBoxId = "tbReply"
+            //        },
 
-                    }.ToString())
-                    {
-                        ActivationType = ToastActivationType.Background
-                    },
+            //        new ToastButton("Like", new QueryString()
+            //        {
+            //            { "action", "like" },
+            //            { "conversationId", messageId.ToString() }
 
-                    new ToastButton("View", new QueryString()
-                    {
-                        { "action", "viewImage" },
-                        { "imageUrl", image }
+            //        }.ToString())
+            //        {
+            //            ActivationType = ToastActivationType.Background
+            //        },
 
-                    }.ToString())
-                }
-            };
+            //        new ToastButton("View", new QueryString()
+            //        {
+            //            { "action", "viewImage" },
+            //            { "imageUrl", image }
+
+            //        }.ToString())
+            //    }
+            //};
 
 
-            ToastContent toastContent = new ToastContent()
-            {
-                Visual = visual
-            };
+            //ToastContent toastContent = new ToastContent()
+            //{
+            //    Visual = visual,
+            //    Launch = new QueryString()
+            //    {
+            //        { "action", "viewConversation" },
+            //        { "conversationId", messageId.ToString() }
 
-            // And create the toast notification
-            var toast = new ToastNotification(toastContent.GetXml());
+            //    }.ToString()
 
+            //};
+
+            //          And create the toast notification
+            // var toast = new ToastNotification(toastContent.GetXml());
+
+            //ToastNotificationManager.CreateToastNotifier().Show(toast);
+
+
+            var text = tc.GetContent();
+            var xml = tc.GetXml();
+            var toast = new ToastNotification(xml);
             ToastNotificationManager.CreateToastNotifier().Show(toast);
+
+
+
+
+            #endregion
+
+
+
+
+            //var xmdock = CreateToast();
+            //var toast = new ToastNotification(xmdock);
+            //var notifi = Windows.UI.Notifications.ToastNotificationManager.CreateToastNotifier();
+            //notifi.Show(toast);
+
+
+
             return messageId;
         }
 
 
+        public static Windows.Data.Xml.Dom.XmlDocument CreateToast()
+        {
+            var xDoc = new XDocument(
+               new XElement("toast",
+               new XElement("visual",
+               new XElement("binding", new XAttribute("template", "ToastGeneric"),
+               new XElement("text", "C# Corner"),
+               new XElement("text", "Do you got MVP award?")
+            )
+            ),// actions    
+            new XElement("actions",
+            new XElement("action", new XAttribute("activationType", "background"),
+            new XAttribute("content", "Yes"), new XAttribute("arguments", "yes")),
+            new XElement("action", new XAttribute("activationType", "background"),
+            new XAttribute("content", "No"), new XAttribute("arguments", "no"))
+            )
+            )
+            );
 
+            var xmlDoc = new Windows.Data.Xml.Dom.XmlDocument();
+            xmlDoc.LoadXml(xDoc.ToString());
+            return xmlDoc;
+        }
 
-    }
+    } 
 
 }

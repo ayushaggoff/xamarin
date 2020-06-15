34e5d0f636e65d963e95bc1aa6e4190949624c3c
Ayush Aggarwal <ayusha@successive.com>
2020-06-15 15:11:24 +0530

    added notification in uwp xamarin form

--- MApp/MApp/MApp.UWP/UWPNotificationManager.cs
+++ MApp/MApp/MApp.UWP/UWPNotificationManager.cs
@@ -1,23 +1,31 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Notifications;
+using Microsoft.QueryStringDotNET; // QueryString.NET
 
 
 
-
+[assembly: Xamarin.Forms.Dependency(typeof(MApp.UWP.UWPNotificationManager))]
 namespace MApp.UWP
 {
     public class UWPNotificationManager : INotificationManager
     {
-        public event EventHandler NotificationReceived;
+        string title = "Ayush  sent you a picture";
+        string content = "Check this out, Happy Canyon in Utah!";
+        string image = "https://picsum.photos/360/202?image=883";
+        string logo = "ms-appdata:///local/Andrew.jpg";
+
+
         int messageId = -1;
+        public event EventHandler NotificationReceived;
+
         public void Initialize()
         {
-            throw new NotImplementedException();
         }
 
         public void ReceiveNotification(string title, string message)
@@ -27,29 +35,97 @@ namespace MApp.UWP
 
         public int ScheduleNotification(string title, string message)
         {
-                 messageId++;
-                string TOAST = $@"<toast>
-                  <visual>
-                    <binding template='ToastGeneric'>
-                      <text>{title}</text>
-                      <text>{message}</text>
-                    </binding>
-                  </visual>
-                  <audio src =""ms-winsoundevent:Notification.Mail"" loop=""true""/>
-                </toast>";
+            ToastVisual visual = new ToastVisual()
+            {
+                BindingGeneric = new ToastBindingGeneric()
+                {
+                    Children =
+                    {
+                        new AdaptiveText()
+                        {
+                            Text = title
+                        },
 
-                Windows.Data.Xml.Dom.XmlDocument xml = new Windows.Data.Xml.Dom.XmlDocument();
-                xml.LoadXml(TOAST);
+                        new AdaptiveText()
+                        {
+                            Text = content
+                         },
 
-                ScheduledToastNotification toast = new ScheduledToastNotification(xml, DateTime.Now + TimeSpan.FromSeconds(3));
-                toast.Id = "IdTostone" + messageId.ToString();
-                toast.Tag = "NotificationOne";
-                toast.Group = nameof(UWPNotificationManager);
-                ToastNotificationManager.CreateToastNotifier().AddToSchedule(toast);
+                        new AdaptiveImage()
+                         {
+                             Source = image
+                        }
+                      },
+                    AppLogoOverride = new ToastGenericAppLogo()
+                    {
+                        Source = logo,
+                        HintCrop = ToastGenericAppLogoCrop.Circle
+                    }
+                }
+            };
 
-                return messageId;
-            }
-            
+
+            ToastActionsCustom actions = new ToastActionsCustom()
+            {
+                Inputs =
+                {
+                    new ToastTextBox("tbReply")
+                    {
+                        PlaceholderContent = "Type a response"
+                    }
+                },
+                Buttons =
+                {
+                    new ToastButton("Reply", new QueryString()
+                    {
+                        { "action", "reply" },
+                        { "conversationId", messageId.ToString() }
+
+                    }.ToString())
+                    {
+                        ActivationType = ToastActivationType.Background,
+                        ImageUri = "Assets/Reply.png",
+ 
+                        // Reference the text box's ID in order to
+                        // place this button next to the text box
+                        TextBoxId = "tbReply"
+                    },
+
+                    new ToastButton("Like", new QueryString()
+                    {
+                        { "action", "like" },
+                        { "conversationId", messageId.ToString() }
+
+                    }.ToString())
+                    {
+                        ActivationType = ToastActivationType.Background
+                    },
+
+                    new ToastButton("View", new QueryString()
+                    {
+                        { "action", "viewImage" },
+                        { "imageUrl", image }
+
+                    }.ToString())
+                }
+            };
+
+
+            ToastContent toastContent = new ToastContent()
+            {
+                Visual = visual
+            };
+
+            // And create the toast notification
+            var toast = new ToastNotification(toastContent.GetXml());
+
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+            return messageId;
+        }
+
+
+
+
     }
 
 }
